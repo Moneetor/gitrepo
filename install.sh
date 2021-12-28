26c52688cb7a4dc76b560784793c5ff7fa24a6a1
moneetor <moneetor@mon.net.pl>
2021-12-28 15:25:26 +0100

    chmod

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -e ~/bin ]; then
     echo "Installing Python files..."
-    chmod u+x ./git-init
-    ln -s ./git-init ~/bin/git-init
+    chmod u+x ./git-repo.py
+    ln -s ./git-repo.py ~/bin/git-init
 fi
