97438332e5fdccd6496aa4897e6aec794cb78d00
moneetor <moneetor@mon.net.pl>
2022-11-16 16:47:44 +0100

    Client side option for creating local repo

--- git-repo.py
+++ git-repo.py
@@ -3,14 +3,41 @@
 import argparse
 import os
 # from sys import stderr
+from urllib.parse import urljoin
+
+
+def git_init_repo(directory=''):
+    destination_dir = os.path.join(os.getcwd(), directory)
+    print("Destination dir: {}".format(destination_dir))
+    os.mkdir(destination_dir)
+    os.chdir(destination_dir)
+    os.system('git init --bare --shared')
+
+
+def git_init_local(directory='', remote_path=''):
+    destination_dir = os.path.join(os.getcwd(), directory)
+    print("Destination dir: {}".format(destination_dir))
+    os.mkdir(destination_dir)
+    os.chdir(destination_dir)
+    os.system('git init --bare --shared')
+    origin = urljoin(remote_path, directory)
+    os.system('git remote add origin {}'.format(origin))
+    os.system('git add .')
+    os.system("git commit -m 'Start project'")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Git init helper')
     parser.add_argument('directory', type=str, help='destination directory')
+    parser.add_argument(
+        '-c',
+        type=bool,
+        default=False,
+        action='store_true',
+        dest='client',
+        help='Create repository at client side'
+    )
     args = parser.parse_args()
-    destinationDir = os.path.join(os.getcwd(), args.directory)
-    print("Destination dir: {}".format(destinationDir))
-    os.mkdir(destinationDir)
-    os.chdir(destinationDir)
-    os.system('git init --bare --shared')
+    if args.client:
+        remote_url = input('Base path on remote (in. e. ssh://login@example.com/yopur/repo/)')
+        git_init_local(args.directory, remote_url)
